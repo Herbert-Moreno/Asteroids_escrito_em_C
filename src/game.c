9702fad563bcdbd23446e06fffd9ebf8af55d8a1
ClockWritesDev <clockwritestwitter@gmail.com>
2025-01-29 10:24:31 -0300

    concertado o raylib novamente ksksks

---
+++ src/game.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <game.h>
+#include <raylib.h>
+
+void game_init(void){
+  printf("Game Initializing...\n");
+}
+
+void game_update(void){
+  //game_draw();
+}
+
+void game_draw(void) {
+  printf("Game Drawing...\n");
+}
