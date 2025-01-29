977537ad1f3b631d5e8b4b8427d8692216ebdffb
ClockWritesDev <clockwritestwitter@gmail.com>
2025-01-29 10:23:28 -0300

    concertado o raylib novamente ksksks

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <raylib.h>
+#include <game.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
 
 int main(void) {
-    printf("Hello, World!\n");
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Game");
+    SetTargetFPS(60);
+    while(!WindowShouldClose()){
+        BeginDrawing();
+            ClearBackground(BLACK);
+            //game_draw();
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
